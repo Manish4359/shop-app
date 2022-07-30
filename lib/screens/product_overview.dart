f52e62aa9f9d27c91aeb5af2d05b63ce468eb8a0
Manish4359 <manishkr.official4359@gmail.com>
2022-07-30 15:59:10 +0530

    app model added

---
+++ lib/screens/product_overview.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ProductOverview extends StatelessWidget {
+  const ProductOverview({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
